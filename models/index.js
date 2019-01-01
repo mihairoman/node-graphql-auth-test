70224228d4b5975a5f4b290b2c1f722d21d2c340
Mihai Roman <mihairoman.r@gmail.com>
2019-01-01 22:36:52 +0200

    Added associations (table relations) to the dbmodels

--- models/index.js
+++ models/index.js
@@ -6,19 +6,21 @@ const sequelize = new Sequelize(
     'pass',
     {
         host: 'localhost',
-        dialect: 'postgres'
-    }
+        dialect: 'postgres',
+    },
 );
 
 const db = {
-    User: sequelize.import('./user')
-}
+    User: sequelize.import('./user'),
+    Board: sequelize.import('./board'),
+    Suggestion: sequelize.import('./suggestion'),
+};
 
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
+Object.keys(db).forEach((modelName) => {
+    if ('associate' in db[modelName]) {
+        db[modelName].associate(db);
+    }
+});
 
 db.sequelize = sequelize;
 // db.Sequelize = Sequelize;
